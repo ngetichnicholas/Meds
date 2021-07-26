609f555e338ae826dfc9918dbdbe639c8a9f32b5
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-26 23:06:39 +0300

    Add profile class model

--- clinic_app/models.py
+++ clinic_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Profile(models.Model):
+  user = models.OneToOneField(User,on_delete=CASCADE)
+  first_name = models.CharField(max_length=144)
+  last_name = models.CharField(max_length=144)
+  email = models.EmailField()
+  profile_picture = CloudinaryField('image')
