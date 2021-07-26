f2bec9a2b942fcf2e267b8066dbf7384c4026251
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-26 23:35:16 +0300

    Add patient class model

--- clinic_app/models.py
+++ clinic_app/models.py
@@ -10,3 +10,15 @@ class Profile(models.Model):
   last_name = models.CharField(max_length=144)
   email = models.EmailField()
   profile_picture = CloudinaryField('image')
+
+GENDER_CHOICES = (
+  ("Male", "Male"),("Female","Female")
+)
+
+class Patient(models.Model):
+  first_name = models.CharField(max_length=30)
+  last_name = models.CharField(max_length=30)
+  id_number = models.IntegerField(blank=True)
+  birth_certificate_no = models.IntegerField(blank=True)
+  gender = models.CharField(max_length=15, choices= GENDER_CHOICES)
+  age = models.IntegerField()
