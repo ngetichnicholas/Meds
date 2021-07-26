bc2eea48386288e5053e388576d4b443a3d0df2e
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-27 00:52:10 +0300

    Set up authentication and models

--- clinic_app/models.py
+++ clinic_app/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.deletion import CASCADE
 from cloudinary.models import CloudinaryField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
 
 # Create your models here.
 class ClinicalStaff(models.Model):
@@ -10,6 +14,15 @@ class ClinicalStaff(models.Model):
   last_name = models.CharField(max_length=144)
   email = models.EmailField()
   profile_picture = CloudinaryField('image')
+
+  def __str__(self):
+    return self.user.username
+
+@receiver(post_save, sender=User)
+def update_staff_signal(sender, instance, created, **kwargs):
+  if created:
+      ClinicalStaff.objects.create(staff=instance)
+  instance.clinicalstaff.save()
 
 GENDER_CHOICES = (
   ("Male", "Male"),("Female","Female")
