405e2298c0e3448153f4e78e430f2b6c24533106
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-27 00:17:59 +0300

    Add index view

--- clinic_app/views.py
+++ clinic_app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+@login_required
+def index(request):
+  current_user = request.user
+  return render(request, 'index.html',{'current_user':current_user})
